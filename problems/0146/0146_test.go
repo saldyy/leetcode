dc6b5992f832cf5a71968738e36e74ba3d07fa1d
Phillip Nguyen <png9981@gmail.com>
2024-10-13 15:00:30 +0700

    chore: uncomment

--- problems/0146/0146_test.go
+++ problems/0146/0146_test.go
@@ -4,50 +4,50 @@ import (
 	"testing"
 )
 
-// func TestSolution146_1(t *testing.T) {
-// 	cache := Constructor(1)
-// 	cache.Put(2, 1)
-//
-// 	result := cache.Get(2)
-// 	if result != 1 {
-// 		t.Errorf("Expected 1 when getting 2 but got %v", result)
-// 	}
-// }
+func TestSolution146_1(t *testing.T) {
+	cache := Constructor(1)
+	cache.Put(2, 1)
 
-// func TestSolution146_2(t *testing.T) {
-// 	cache := Constructor(2)
-// 	cache.Put(1, 1)
-// 	cache.Put(2, 2)
-//
-// 	result := cache.Get(1)
-// 	if result != 1 {
-// 		t.Errorf("Expected 1 when getting 1 but got %v", result)
-// 	}
-//
-// 	cache.Put(3, 3)
-//
-// 	result = cache.Get(2)
-// 	if result != -1 {
-// 		t.Errorf("Expected -1 when getting 2 but got %v", result)
-// 	}
-//
-// 	cache.Put(4, 4)
-//
-// 	result = cache.Get(1)
-// 	if result != -1 {
-// 		t.Errorf("Expected -1 when getting 1 but got %v", result)
-// 	}
-//
-// 	result = cache.Get(3)
-// 	if result != 3 {
-// 		t.Errorf("Expected 3 when getting 3 but got %v", result)
-// 	}
-//
-// 	result = cache.Get(4)
-// 	if result != 4 {
-// 		t.Errorf("Expected 4 when getting 4 but got %v", result)
-// 	}
-// }
+	result := cache.Get(2)
+	if result != 1 {
+		t.Errorf("Expected 1 when getting 2 but got %v", result)
+	}
+}
+
+func TestSolution146_2(t *testing.T) {
+	cache := Constructor(2)
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+
+	result := cache.Get(1)
+	if result != 1 {
+		t.Errorf("Expected 1 when getting 1 but got %v", result)
+	}
+
+	cache.Put(3, 3)
+
+	result = cache.Get(2)
+	if result != -1 {
+		t.Errorf("Expected -1 when getting 2 but got %v", result)
+	}
+
+	cache.Put(4, 4)
+
+	result = cache.Get(1)
+	if result != -1 {
+		t.Errorf("Expected -1 when getting 1 but got %v", result)
+	}
+
+	result = cache.Get(3)
+	if result != 3 {
+		t.Errorf("Expected 3 when getting 3 but got %v", result)
+	}
+
+	result = cache.Get(4)
+	if result != 4 {
+		t.Errorf("Expected 4 when getting 4 but got %v", result)
+	}
+}
 
 func TestSolution146_3(t *testing.T) {
 	cache := Constructor(2)
