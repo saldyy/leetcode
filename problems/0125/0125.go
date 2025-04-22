9e8557d4f3a12b052e44104c08d69d9c9732cfc7
Phillip Nguyen <png9981@gmail.com>
2025-04-22 14:39:02 +0700

    feat: add problem 125

---
+++ problems/0125/0125.go
@@ -0,0 +1,53 @@
+package problem125
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func formatChar(b byte) (bool, byte) {
+	if b >= 48 && b <= 57 {
+		return true, b
+	}
+	if b >= 65 && b <= 90 {
+		return true, b + 32
+	}
+	if b >= 97 && b <= 122 {
+		return true, b
+	}
+
+	return false, b
+}
+
+func isPalindrome(s string) bool {
+	var sb strings.Builder
+	for i := range s {
+		isAscii, fb := formatChar(s[i])
+		if !isAscii {
+			continue
+		}
+		sb.WriteByte(fb)
+	}
+
+	fs := sb.String()
+	left, right := 0, len(fs)-1
+
+	fmt.Printf("S: %s\n", s)
+	fmt.Printf("Formatted s: %s\n", fs)
+
+	for left <= right {
+		if fs[left] != fs[right] {
+			return false
+		}
+		left++
+		right--
+	}
+
+	return true
+}
+
+func Execute(a string) bool {
+
+	return isPalindrome(a)
+}
---
+++ problems/0125/0125_test.go
@@ -0,0 +1,60 @@
+package problem125
+
+import "testing"
+
+type Input struct {
+	s string
+}
+
+type Case struct {
+	name     string
+	input    Input
+	expected bool
+}
+
+func TestExecute(t *testing.T) {
+	cases := []Case{
+    {
+      name: "Case 001",
+      input: Input{
+        s: " ",
+      },
+      expected: true,
+    },
+    {
+      name: "Case 002",
+      input: Input{
+        s: "A man, a plan, a canal: Panama",
+      },
+      expected: true,
+    },
+    {
+      name: "Case 003",
+      input: Input{
+        s: "race a car",
+      },
+      expected: false,
+    },
+    {
+      name: "Case 004",
+      input: Input{
+        s: "0P",
+      },
+      expected: false,
+    },
+  }
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			input := c.input
+			result := Execute(input.s)
+			if result != c.expected {
+				t.Errorf("\x1b[31mFAILED %s: got=%v, want=%v\x1b[0m",
+					c.name, result, c.expected)
+			} else {
+				t.Logf("\x1b[32mPASSED %s: got=%v\x1b[0m",
+					c.name, result)
+			}
+		})
+	}
+}
